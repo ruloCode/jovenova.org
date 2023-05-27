74433ebe88181f14458c2b02dfcf3a49bcd021e3
rulocode <andres@vitau.mx>
2023-05-27 14:03:10 -0500

    without prisma

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -7,13 +7,13 @@ import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
 
-export default function NavBar({ session }: { session: Session | null }) {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
+export default function NavBar() {
+  // const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
 
   return (
     <>
-      <SignInModal />
+      {/* <SignInModal /> */}
       <div
         className={`fixed top-0 w-full ${
           scrolled
